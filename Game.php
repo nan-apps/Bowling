6af1b59ce89146c05b8629b6d95f9e6ba4fc8e1e
Gaston Nan <nangastonr@gmail.com>
2018-06-24 18:35:57 -0300

    code style fix

--- Game.php
+++ Game.php
@@ -1,58 +1,64 @@
 <?php
 namespace Bowling;
 
-class Game {
+class Game
+{
+    private $rolls = [];
+    private $currentRoll = 0;
 
-	private $rolls = [];
-	private $currentRoll = 0;
-
-	public function __construct(){
-            $this->rolls = array_fill(0, 20, 0);
-	}
-
-    public function roll(int $pins): void{
-    	$this->rolls[$this->currentRoll++] = $pins;
+    public function __construct()
+    {
+        $this->rolls = array_fill(0, 20, 0);
     }
 
-    public function score(): int{
-    	$score = 0;
-    	$firstBallInFrame = 0;
-
-    	for ($frame=0; $frame<10; $frame++) { 
-
-    		if( $this->isStrike($firstBallInFrame) ){
-    			$score += 10 + $this->nextTwoBallsForStrike($firstBallInFrame);
-    			$firstBallInFrame += 1;
-    		} elseif ( $this->isSpare($firstBallInFrame) ){
-    			$score += 10 + $this->nextBallForSpare($firstBallInFrame);
-    			$firstBallInFrame += 2;
-    		} else {
-    			$score += $this->nextBallsInFrame($firstBallInFrame);
-	    		$firstBallInFrame += 2;
-    		}
-    	}
-
-    	return $score;
+    public function roll(int $pins): void
+    {
+        $this->rolls[$this->currentRoll++] = $pins;
     }
 
-    private function nextTwoBallsForStrike($firstBallInFrame): int{
-    	return $this->rolls[$firstBallInFrame+1] + $this->rolls[$firstBallInFrame+2];
+    public function score(): int
+    {
+        $score = 0;
+        $firstBallInFrame = 0;
+
+        for ($frame=0; $frame<10; $frame++) {
+            if ($this->isStrike($firstBallInFrame)) {
+                $score += 10 + $this->nextTwoBallsForStrike($firstBallInFrame);
+                $firstBallInFrame += 1;
+            } elseif ($this->isSpare($firstBallInFrame)) {
+                $score += 10 + $this->nextBallForSpare($firstBallInFrame);
+                $firstBallInFrame += 2;
+            } else {
+                $score += $this->nextBallsInFrame($firstBallInFrame);
+                $firstBallInFrame += 2;
+            }
+        }
+
+        return $score;
     }
 
-    private function nextBallForSpare($firstBallInFrame): int{
-    	return $this->rolls[$firstBallInFrame+2];
+    private function nextTwoBallsForStrike($firstBallInFrame): int
+    {
+        return $this->rolls[$firstBallInFrame+1] + $this->rolls[$firstBallInFrame+2];
     }
 
-    private function nextBallsInFrame($firstBallInFrame): int{
-    	return $this->rolls[$firstBallInFrame] + $this->rolls[$firstBallInFrame+1];
+    private function nextBallForSpare($firstBallInFrame): int
+    {
+        return $this->rolls[$firstBallInFrame+2];
     }
 
-    private function isSpare($firstBallInFrame): bool{
-    	return $this->rolls[$firstBallInFrame] + $this->rolls[$firstBallInFrame+1] == 10;
+    private function nextBallsInFrame($firstBallInFrame): int
+    {
+        return $this->rolls[$firstBallInFrame] + $this->rolls[$firstBallInFrame+1];
     }
 
-    private function isStrike($firstBallInFrame): bool{
-    	return $this->rolls[$firstBallInFrame]  == 10;
+    private function isSpare($firstBallInFrame): bool
+    {
+        return $this->rolls[$firstBallInFrame] + $this->rolls[$firstBallInFrame+1] == 10;
     }
 
+    private function isStrike($firstBallInFrame): bool
+    {
+        return $this->rolls[$firstBallInFrame]  == 10;
+    }
 }
--- tests/GameTest.php
+++ tests/GameTest.php
@@ -3,57 +3,64 @@
 use PHPUnit\Framework\TestCase;
 use Bowling\Game;
 
-final class GameTest extends TestCase {
+final class GameTest extends TestCase
+{
+    private $game;
 
-	private $game;
-
-	public function setUp(): void {
-		$this->game = new Game();
-	}
+    public function setUp(): void
+    {
+        $this->game = new Game();
+    }
     
-    private function rollMany(int $n, int $pins): void{
-    	for ($i=0; $i<$n; $i++) { 
-        	$this->game->roll($pins);
+    private function rollMany(int $n, int $pins): void
+    {
+        for ($i=0; $i<$n; $i++) {
+            $this->game->roll($pins);
         }
     }
 
-    private function rollSpare(): void{
-    	$this->rollMany(2, 5);
+    private function rollSpare(): void
+    {
+        $this->rollMany(2, 5);
     }
 
-    private function rollStrike(): void{
-    	$this->game->roll(10);
+    private function rollStrike(): void
+    {
+        $this->game->roll(10);
     }
    
-    public function testGutterGame(): void {
+    public function testGutterGame(): void
+    {
         $this->rollMany(20, 0);
         $this->assertEquals(0, $this->game->score());
     }
 
-    public function testAllOnes(): void {
+    public function testAllOnes(): void
+    {
         $this->rollMany(20, 1);
         $this->assertEquals(20, $this->game->score());
     }
 
-    public function testOneSpare(): void {
-    	$this->rollSpare();
-    	$this->game->roll(3); //despues de un spare suma doble
+    public function testOneSpare(): void
+    {
+        $this->rollSpare();
+        $this->game->roll(3); //despues de un spare suma doble
         $this->rollMany(17, 0);
         $this->assertEquals(16, $this->game->score());
     }
 
-    public function testOneStrike(): void {
-    	$this->rollStrike();
-    	$this->game->roll(3); //suma doble
-    	$this->game->roll(4); //suma doble
-    	$this->rollMany(16, 0);
+    public function testOneStrike(): void
+    {
+        $this->rollStrike();
+        $this->game->roll(3); //suma doble
+        $this->game->roll(4); //suma doble
+        $this->rollMany(16, 0);
         $this->assertEquals(24, $this->game->score());
     }
 
-    public function testPerfectGame(): void {
-    	$this->rollMany(12, 10);
+    public function testPerfectGame(): void
+    {
+        $this->rollMany(12, 10);
         $this->assertEquals(300, $this->game->score());
     }
-
-    
 }
